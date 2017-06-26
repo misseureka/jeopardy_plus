b073e19dada7e5a1ef11cc448949331b60d8fce1
Alex Trostky <alexbecoon@gmail.com>
2017-06-26 22:35:19 +0300

    draft

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,37 @@
-(function(){
-  var app = angular.module("app", []);
-  app.controller("jeopardyController", function($scope) {
-    $scope.categories = window.categories;
+(() => {
+  var app = angular.module("app", ["ui.bootstrap.modal"]);
+    app.controller('jeopardyController', ($scope) => {
     $scope.costs = window.costs;
+    $scope.board = window.board;
+    $scope.players = [];
 
-    $scope.getQuestion = function(category, cost){
-      return $scope.questions.filter(function(q){
-        return q.categoryId === category.id && q.cost === cost;
-      })[0].question;
-    }
+    var getQuestion = (parrentIdx, idx) => {
+      return $scope.board[parrentIdx].questions[idx];
+    };
+
+    $scope.showQuestion = (parrentIndex, index) => {
+      var question = getQuestion(parrentIndex, index);
+      var questionText = question.question;
+      question.isActive = false;
+      $scope.showModal = true;
+      $scope.currentQuestion = question;
+    };
+
+    $scope.ok = function() {
+      $scope.showModal = false;
+      $scope.currentQuestion = null;
+    };
   });
 })();
+
+//app.factory("UserService", function() {
+//    var items = ["Peter", "Daniel", "Nina"];
+//    return {
+//      all: function() {
+//        return items;
+//      },
+//      first: function() {
+//        return items[0];
+//      }
+//    };
+//  });
--- scripts/schema.js
+++ scripts/schema.js
@@ -1,45 +1,128 @@
-window.questions = [
+window.board = [
   {
-    categoryId: 1,
-    cost: 100,
-    question: 'Вопрос 2',
-    questionType: 'text',
-    answerType: null
+    category: "Издевательская тема",
+    questions: [
+        {
+          cost: 100,
+          question: 'Внешняя политика Черногории.',
+          questionType: 'text',
+          answer: null,
+          isActive: true
+        },
+        {
+          cost: 200,
+          question: 'Нефирменный вагон фирменного поезда.',
+          questionType: 'text',
+          answer: {
+            type: 'img',
+            source: './data/images/1_200'
+          },
+          isActive: true
+        },
+        {
+          cost: 300,
+          question: 'Шуточная просьба.',
+          questionType: 'text',
+          answer: null,
+          isActive: true
+        },
+        {
+          cost: 400,
+          question: 'Сложная ситуация, связанная с отношениями с людьми.',
+          questionType: 'text',
+          answer: {
+            type: 'video',
+            source: './data/video/1_400'
+          },
+          isActive: true
+        },
+        {
+          cost: 500,
+          question: 'Дом для уточки.',
+          questionType: 'text',
+          answer: null,
+          isActive: true
+        }
+    ]
   },
   {
-    categoryId: 1,
-    cost: 200,
-    question: 'Вопрос 1_2',
-    questionType: 'text',
-    answerType: 'img',
-    answerImageSrc: ''
+    category: "Вопросы без ответов",
+    questions: [
+        {
+          cost: 100,
+          question: 'Игристое на завтрак.',
+          questionType: 'text',
+          answer: null,
+          isActive: true
+        },
+        {
+          cost: 200,
+          question: 'Субботняя поездка к морю.',
+          questionType: 'text',
+          answer: null,
+          isActive: true
+        },
+        {
+          cost: 300,
+          question: 'Маленькая бетономешалка.',
+          questionType: 'text',
+          answer: null,
+          isActive: true
+        },
+        {
+          cost: 400,
+          question: 'Хорошая шутка, над которой не посмеялись.',
+          questionType: 'text',
+          answer: null,
+          isActive: true
+        },
+        {
+          cost: 500,
+          question: 'Футболка с изображением хорошей рок-группы.',
+          questionType: 'text',
+          answer: null,
+          isActive: true
+        }
+    ]
   },
   {
-    categoryId: 2,
-    cost: 200,
-    question: 'Вопрос 2_!',
-    questionType: 'text',
-    answerType: null
-  },
-  {
-    categoryId: 2,
-    cost: 100,
-    question: 'Вопрос 2',
-    questionType: 'text',
-    answerType: 'video',
-    answerVideoSrc: ''
+    category: "Автор, убей себя!",
+    questions: [
+        {
+          cost: 100,
+          question: 'Ситуация только с неприятными исходами.',
+          questionType: 'text',
+          answer: null,
+          isActive: true
+        },
+        {
+          cost: 200,
+          question: 'Краткий дневной сон (не более 10 минут).',
+          questionType: 'text',
+          answer: null,
+          isActive: true
+        },
+        {
+          cost: 300,
+          question: 'Милиционер, не повышенный в должности',
+          questionType: 'text',
+          answer: null,
+          isActive: true
+        },
+        {
+          cost: 400,
+          question: 'Сильное, но преодолимое желание помыть пол',
+          questionType: 'text',
+          answer: null,
+          isActive: true
+        },
+        {
+          cost: 500,
+          question: 'Грязь, которую, сначала кажется, невозможно отмыть',
+          questionType: 'text',
+          answer: null,
+          isActive: true
+        }
+    ]
   }
 ];
-
-window.categories = [
-  {
-    id: 1,
-    name: 'Первая'
-  },
-  {
-    id: 2,
-    name: 'Втораяы'
-  }
-];
-
-window.costs = [100, 200];
