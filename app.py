bc382e4e052494fa9d070e918485515a8a09d539
Alex Trostky <alexbecoon@gmail.com>
2017-07-03 21:02:17 +0300

    Host and players basic interaction via socket

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import asyncio
+from asyncio import Lock
 
 from aiohttp import web
 
@@ -7,17 +7,38 @@ import socketio
 sio = socketio.AsyncServer(async_mode='aiohttp')
 app = web.Application()
 sio.attach(app)
+lock = Lock()
 
 
-async def background_task():
-    """Example of how to send server generated events to clients."""
-    count = 0
-    while True:
-        await sio.sleep(10)
-        count += 1
-        await sio.emit('jeo_response', {'data': 'Server generated event'},
-                        namespace='')
+class Player(object):
+    def __init__(self, name, score, sid):
+        self.name = name
+        self.score = score
+        self.sid = sid
 
+
+class Jeopardy(object):
+    players = []
+    is_game_started = False
+    is_host_connected = False
+    already_answered = False
+    host_sid = None
+
+    @classmethod
+    def push_button(cls):
+        if lock.locked() or cls.already_answered:
+            return False
+
+        with (yield from lock):
+            if not cls.already_answered:
+                cls.already_answered = True
+                return True
+        return False
+
+    @classmethod
+    def get_player(cls, sid):
+        p = next(x for x in cls.players if x.sid == sid)
+        return p
 
 async def index(request):
     print('Index called')
@@ -25,59 +46,55 @@ async def index(request):
         return web.Response(text=f.read(), content_type='text/html')
 
 
-@sio.on('my event', namespace='/test')
-async def test_message(sid, message):
-    await sio.emit('my response', {'data': message['data']}, room=sid,
-                   namespace='/test')
+@sio.on('login', namespace='')
+async def login_message(sid, message):
+    print('Player logged in.')
+    team_name = message['teamName']
+    if team_name == 'host':
+        Jeopardy.is_host_connected = True
+        Jeopardy.host_sid = sid
+    else:
+        player = Player(team_name, 0, sid)
+        Jeopardy.players.append(player)
+    if len(Jeopardy.players) > 3:
+        for player in Jeopardy.players:
+            print('{} {} {}'.format(player.name, player.score, player.sid))
 
 
-@sio.on('my broadcast event', namespace='/test')
-async def test_broadcast_message(sid, message):
-    await sio.emit('my response', {'data': message['data']}, namespace='/test')
+@sio.on('host_started_game', namespace='')
+async def host_started_game_message(sid, message):
+    print('Host started game.')
+    assert sid == Jeopardy.host_sid
+    Jeopardy.is_game_started = True
+    await sio.emit('game_started', namespace='')
 
 
-@sio.on('join', namespace='/test')
-async def join(sid, message):
-    sio.enter_room(sid, message['room'], namespace='/test')
-    await sio.emit('my response', {'data': 'Entered room: ' + message['room']},
-                   room=sid, namespace='/test')
+@sio.on('player_pushed_button', namespace='')
+async def player_pushed_button(sid, message):
+    p = Jeopardy.get_player(sid)
+    if Jeopardy.push_button():
+        print('Player {} was the first one'.format(p.name))
+        return True
+    return False
 
 
-@sio.on('leave', namespace='/test')
-async def leave(sid, message):
-    sio.leave_room(sid, message['room'], namespace='/test')
-    await sio.emit('my response', {'data': 'Left room: ' + message['room']},
-                   room=sid, namespace='/test')
-
-
-@sio.on('close room', namespace='/test')
-async def close(sid, message):
-    await sio.emit('my response',
-                   {'data': 'Room ' + message['room'] + ' is closing.'},
-                   room=message['room'], namespace='/test')
-    await sio.close_room(message['room'], namespace='/test')
-
-
-@sio.on('my room event', namespace='/test')
-async def send_room_message(sid, message):
-    await sio.emit('my response', {'data': message['data']},
-                   room=message['room'], namespace='/test')
-
-
-@sio.on('disconnect request', namespace='/test')
+@sio.on('disconnect request', namespace='')
 async def disconnect_request(sid):
+    # TODO: show team name
+    # print('Disconnect request from sid: {}'.format(sid))
     await sio.disconnect(sid, namespace='/test')
 
 
-@sio.on('connect', namespace='/test')
-async def test_connect(sid, environ):
-    await sio.emit('my response', {'data': 'Connected', 'count': 0}, room=sid,
-                   namespace='/test')
+@sio.on('connect', namespace='')
+async def connect(sid, environ):
+    # print('Handled connect from sid {}, environ: {}'.format(sid, environ))
+    await sio.emit('jeo_response', {'data': 'Connected', 'count': 0}, room=sid,
+                   namespace='')
 
 
-@sio.on('disconnect', namespace='/test')
-def test_disconnect(sid):
-    print('Client disconnected')
+@sio.on('disconnect', namespace='')
+def disconnect(sid):
+    print('Client {} disconnected'.format(sid))
 
 
 app.router.add_static('/scripts', 'scripts')
@@ -85,5 +102,5 @@ app.router.add_get('/', index)
 
 
 if __name__ == '__main__':
-    sio.start_background_task(background_task)
+    # sio.start_background_task(background_task)
     web.run_app(app, host='127.0.0.1', port=5000)
--- scripts/app.js
+++ scripts/app.js
@@ -11,22 +11,35 @@
   });
 
   app.controller('jeopardyController', ($scope, JeoSocket) => {
-    $scope.costs = window.costs;
     $scope.board = window.board;
-    $scope.players = [];
+    $scope.showStartButton = false;
+    $scope.showLogin = false;
+    $scope.waitForStart = false;
+    $scope.isMainBoard = false;
+
     JeoSocket.on('connect', function() {
       console.log('Connect!!!');
+      $scope.showLogin = true;
     });
     JeoSocket.on('jeo_response', (resp) => {
       console.log('Got response' + resp);
-    })
+    });
+
+    JeoSocket.on('game_started', (resp) => {
+      $scope.waitForStart = false;
+      console.log('game_started');
+    });
+
+    JeoSocket.on('host_connected', (resp) => {
+      console.log('host_connected');
+      console.log(resp);
+    });
 
     var getQuestion = (parrentIdx, idx) => {
       return $scope.board[parrentIdx].questions[idx];
     };
 
     $scope.showQuestion = (parrentIndex, index) => {
-      debugger;
       var question = getQuestion(parrentIndex, index);
       var questionText = question.question;
       question.isActive = false;
@@ -38,17 +51,28 @@
       $scope.showModal = false;
       $scope.currentQuestion = null;
     };
+
+    $scope.login = function(teamName){
+      JeoSocket.emit('login', {'teamName': teamName});
+      $scope.showLogin = false;
+      $scope.teamName = teamName;
+      $scope.isMainBoard = teamName == 'host';
+      if (!$scope.isMainBoard){
+        $scope.waitForStart = true;
+      } else {
+        $scope.showStartButton = true;
+      }
+    };
+
+    $scope.startGame = function(){
+      JeoSocket.emit('host_started_game', null);
+    };
+
+    $scope.push_button = function(){
+      JeoSocket.emit('player_pushed_button', function(data){
+        console.log('You have pushed the button!')
+        console.log(data);
+      });
+    };
   });
 })();
-
-//app.factory("UserService", function() {
-//    var items = ["Peter", "Daniel", "Nina"];
-//    return {
-//      all: function() {
-//        return items;
-//      },
-//      first: function() {
-//        return items[0];
-//      }
-//    };
-//  });
